a4aa21b219ce80114aa970843a20b4596382113e
Akhtar Faizi Putra <akhtarerror@gmail.com>
2024-10-07 14:54:08 +0700

    first commit

---
+++ app/src/main/java/bangkit/mobiledev/aplikasidicodingeventnavigationdanapi/MainActivity.kt
@@ -0,0 +1,25 @@
+package bangkit.mobiledev.aplikasidicodingeventnavigationdanapi
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        // Retrieve the NavController from the NavHostFragment
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        // Set up bottom navigation with NavController
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        bottomNav.setupWithNavController(navController)
+    }
+}
